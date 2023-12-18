3d5a92a545b7b80929a26851a18e57eb31052b49
JJOmin <104137706+JJOmin@users.noreply.github.com>
2023-12-18 22:41:26 +0100

    Forst Idea for Selection of Rows and Cols

--- Gaussy/Gaussy/ContentView.swift
+++ Gaussy/Gaussy/ContentView.swift
@@ -4,30 +4,39 @@ import SwiftUI
 
 struct ContentView: View {
     @ObservedObject var viewModel: ViewModel
-
+    
     init(viewModel: ViewModel) {
         self.viewModel = viewModel
     }
-
+    
     var body: some View {
-        VStack{
-            VStack{
-                Text("Gaussy Game")
+        VStack {
+            VStack {
+                Text("Gaussy Game").font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
             }
-            VStack(spacing: interSquareSpacing(for: viewModel.matrix)) {
-                
-                ForEach(0..<viewModel.matrix.count, id: \.self) { row in
-                    HStack(spacing: interSquareSpacing(for: viewModel.matrix)) {
-                        ForEach(0..<viewModel.matrix[row].count, id: \.self) { col in
-                            Text("\(viewModel.matrix[row][col])")
-                                .font(.system(size: fontSize(for: viewModel.matrix)))
-                                .frame(width: squareSize(for: viewModel.matrix), height: squareSize(for: viewModel.matrix))
-                                .border(Color.black) // Optional: Rahmen um das Quadrat hinzufügen
-                                .padding(interSquareSpacing(for: viewModel.matrix)) // Optional: Innenabstand des Quadrats
+            HStack {
+                VStack(spacing: interSquareSpacing(for: viewModel.matrix)) {
+                    ForEach(0..<viewModel.matrix.count, id: \.self) { row in
+                        HStack(spacing: interSquareSpacing(for: viewModel.matrix)) {
+                            Circle()
+                                .fill(viewModel.selectedRow == row ? Color.blue : Color.gray)
+                                .frame(width: 20, height: 20)
+                                .onTapGesture {
+                                    viewModel.selectedRow = row // Set the selected row in the ViewModel
+                                }
+                            
+                            ForEach(0..<viewModel.matrix[row].count, id: \.self) { col in
+                                Text("\(viewModel.matrix[row][col])")
+                                    .font(.system(size: fontSize(for: viewModel.matrix)))
+                                    .frame(width: squareSize(for: viewModel.matrix), height: squareSize(for: viewModel.matrix))
+                                    .border(Color.black) // Optional: Rahmen um das Quadrat hinzufügen
+                                    .padding(interSquareSpacing(for: viewModel.matrix)) // Optional: Innenabstand des Quadrats
+                            }
                         }
                     }
                 }
-            };Spacer()
+            }
+            Spacer()
         }
     }
 }
--- Gaussy/Gaussy/ViewModel.swift
+++ Gaussy/Gaussy/ViewModel.swift
@@ -4,13 +4,16 @@
 //
 //  Created by Leonhard Tilly on 18.12.23.
 //
+// ViewModel.swift
 
 import Foundation
-// ViewModel.swift
+import SwiftUI
 
 class ViewModel: ObservableObject {
     @Published var matrix: [[Int]] = []
+    @Published var selectedRow: Int? // Property to store the selected row
+    
     init() {
-        self.matrix = Model.identityMatrix(size: 2)
+        self.matrix = Model.identityMatrix(size: 3)
     }
 }
